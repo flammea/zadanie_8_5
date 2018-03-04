bbf2435ae1e8fd17789b6642f2874ae51c76c764
Beata Markunas <beata.markunas@gmail.com>
2018-03-04 18:15:28 +0100

    Create basic files and add html and js code

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var a = 2,
+	h = 3,
+	triangleArea;
+triangleArea =a*h/2;
+alert('The result is ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
